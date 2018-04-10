6bcbca85e19671ffc0da65737ce130ff8c8c234b
Lowe_Marklund <lm222qb@student.lnu.se>
2018-04-10 14:00:55 +0200

    Add method createNoteTable

--- src/js/synth.js
+++ src/js/synth.js
@@ -10,6 +10,26 @@ const template = document.createElement('template')
 template.innerHTML =
 `<div class="synth">
     <div class="keyboard">
+      <div class="settingsBar">
+        <div class="left">
+          <span>Volume: </span>
+          <input type="range" min="0.0" max="1.0" step="0.01"
+              value="0.5" list="volumes" name="volume">
+          <datalist id="volumes">
+            <option value="0.0" label="Mute">
+            <option value="1.0" label="100%">
+          </datalist>
+        <div class="right">
+          <span>Current waveform: </span>
+          <select name="waveform">
+            <option value="sine">Sine</option>
+            <option value="square" selected>Square</option>
+            <option value="sawtooth">Sawtooth</option>
+            <option value="triangle">Triangle</option>
+          </select>
+        </div>
+      </div>
+      </div>
     <div>
     <style>
     </style>
@@ -19,7 +39,7 @@ template.innerHTML =
 /**
  * Synth.
  *
- * @class Sequencer
+ * @class Synth
  * @extends {window.HTMLElement}
  */
 
@@ -33,6 +53,16 @@ class Synth extends window.HTMLElement {
     this.attachShadow({mode: 'open'})
     this.shadowRoot.appendChild(template.content.cloneNode(true))
     this._synth = this.shadowRoot.querySelector('.synth')
+    this._keyboard = document.querySelector(".keyboard")
+    this._wavePicker = document.querySelector("select[name='waveform']")
+    this._volumeControl = document.querySelector("input[name='volume']")
+    this._audioContext = new (window.AudioContext || window.webkitAudioContext);
+    this._oscList = []
+    this._masterGainNode = null
+    this._noteFreq = null
+    this._customWaveform = null
+    this._sineTerms = null
+    this._cosineTerms = null
   }
 
   /**
@@ -40,7 +70,7 @@ class Synth extends window.HTMLElement {
    *
    * @readonly
    * @static
-   * @memberof Sequencer
+   * @memberof Synth
    */
   static get observedAttributes () {
     return 
@@ -52,7 +82,7 @@ class Synth extends window.HTMLElement {
    * @param {string} attributeName
    * @param {string} oldValue
    * @param {string} newValue
-   * @memberof Sequencer
+   * @memberof Synth
    */
   attributeChangedCallback (attributeName, oldValue, newValue) {
     
@@ -60,20 +90,48 @@ class Synth extends window.HTMLElement {
     /**
    * Called when the element is connected to the DOM.
    *
-   * @memberof Sequencer
+   * @memberof Synth
    */
   connectedCallback () {
+  
   }
 
      /**
    * Listens after input from the user.
    *
-   * @memberof Sequencer
+   * @memberof Synth
    */
   inputListen () {
     
   }
   
+      /**
+   * Creates array of pitches
+   *
+   * @memberof Synth
+   */
+  createNoteTable(){
+    let noteFreq = [];
+    for (let i=0; i< 9; i++) {
+      noteFreq[i] = [];
+    }
+    noteFreq[1]["C"] = 32.703195662574829
+    noteFreq[1]["C#"] = 34.647828872109012
+    noteFreq[1]["D"] = 36.708095989675945
+    noteFreq[1]["D#"] = 38.890872965260113
+    noteFreq[1]["E"] = 41.203444614108741
+    noteFreq[1]["F"] = 43.653528929125485
+    noteFreq[1]["F#"] = 46.249302838954299
+    noteFreq[1]["G"] = 48.999429497718661
+    noteFreq[1]["G#"] = 51.913087197493142
+    noteFreq[1]["A"] = 55.000000000000000
+    noteFreq[1]["A#"] = 58.270470189761239
+    noteFreq[1]["B"] = 61.735412657015513
+    return noteFreq
+  }
+  
 }
   
+window.customElements.define('synth-element', Synth)
+
 module.exports = Synth
