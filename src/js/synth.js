3cf778f45a12423a47b3d46674441b5baacfcdaa
Lowe_Marklund <lm222qb@student.lnu.se>
2018-04-15 13:51:52 +0200

    Add functionality for changing octave on keyboard

--- src/js/synth.js
+++ src/js/synth.js
@@ -8,8 +8,8 @@
 
  'use strict'
 
-const template = document.createElement('template')
-template.innerHTML =
+ const template = document.createElement('template')
+ template.innerHTML =
 `<div class="synth">
     <div class="keyboard">
       <div class="settingsBar">
@@ -28,6 +28,17 @@ template.innerHTML =
             <option value="square" selected>Square</option>
             <option value="sawtooth">Sawtooth</option>
             <option value="triangle">Triangle</option>
+          </select>
+          <span>Current octave: </span>
+          <select name="octave">
+            <option value="1">1</option>
+            <option value="2" selected>2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6" selected>6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
           </select>
         </div>
       </div>
@@ -49,7 +60,7 @@ template.innerHTML =
       
       .key {
         cursor: pointer;
-        font: 16px "Open Sans", "Lucida Grande", "Arial", sans-serif;
+        font: 10px "Open Sans", "Lucida Grande", "Arial", sans-serif;
         border: 1px solid black;
         border-radius: 5px;
         width: 20px;
@@ -101,6 +112,28 @@ template.innerHTML =
         width: 100%;
         height: 30px;
       }
+
+      .sharpKey {
+        cursor: pointer;
+        font: 10px "Open Sans", "Lucida Grande", "Arial", sans-serif;
+        border: 1px solid black;
+        border-radius: 5px;
+        width: 20px;
+        height: 80px;
+        padding: 10px;
+        text-align: center;
+        box-shadow: 2px 2px darkgray;
+        display: inline-block;
+        position: relative;
+        margin-right: 3px;
+        user-select: none;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+        background-color: black;
+        color: white;
+        margin-bottom: -60px;
+      }
   
     </style>
 </div>
@@ -113,27 +146,28 @@ template.innerHTML =
  * @extends {window.HTMLElement}
  */
 
-class Synth extends window.HTMLElement {
+ class Synth extends window.HTMLElement {
   /**
    * Creates an instance of Memory .
    * @memberof Synth
    */
-  constructor () {
-    super()
-    this.attachShadow({mode: 'open'})
-    this.shadowRoot.appendChild(template.content.cloneNode(true))
-    this._synth = this.shadowRoot.querySelector('.synth')
-    this._keyboard = this.shadowRoot.querySelector(".keyboard")
-    this._wavePicker = this.shadowRoot.querySelector("select[name='waveform']")
-    this._volumeControl = this.shadowRoot.querySelector("input[name='volume']")
-    this._audioContext = new (window.AudioContext || window.webkitAudioContext)
-    this._oscList = []
-    this._masterGainNode = null
-    this._noteFreq = null
-    this._customWaveform = null
-    this._sineTerms = null
-    this._cosineTerms = null
-  }
+   constructor () {
+     super()
+     this.attachShadow({mode: 'open'})
+     this.shadowRoot.appendChild(template.content.cloneNode(true))
+     this._synth = this.shadowRoot.querySelector('.synth')
+     this._keyboard = this.shadowRoot.querySelector('.keyboard')
+     this._wavePicker = this.shadowRoot.querySelector("select[name='waveform']")
+     this._octavePicker = this.shadowRoot.querySelector("select[name='octave']")
+     this._volumeControl = this.shadowRoot.querySelector("input[name='volume']")
+     this._audioContext = new (window.AudioContext || window.webkitAudioContext)()
+     this._oscList = []
+     this._masterGainNode = null
+     this._noteFreq = null
+     this._customWaveform = null
+     this._sineTerms = null
+     this._cosineTerms = null
+   }
 
   /**
    * Watches attributes for changes on the element.
@@ -142,9 +176,9 @@ class Synth extends window.HTMLElement {
    * @static
    * @memberof Synth
    */
-  static get observedAttributes () {
-    return
-  }
+   static get observedAttributes () {
+
+   }
 
   /**
    * Called by the browser engine when an attribute changes.
@@ -154,166 +188,184 @@ class Synth extends window.HTMLElement {
    * @param {string} newValue
    * @memberof Synth
    */
-  attributeChangedCallback (attributeName, oldValue, newValue) {
-    
-  }
+   attributeChangedCallback (attributeName, oldValue, newValue) {
+
+   }
     /**
    * Called when the element is connected to the DOM.
    *
    * @memberof Synth
    */
-  connectedCallback () {
-    this.inputListen()
-    this.setup()
-  }
+   connectedCallback () {
+     this.setup()
+     this.inputListen()
+   }
 
      /**
    * Listens after input from the user.
    *
    * @memberof Synth
    */
-  inputListen () {
-    
-  }
+   inputListen () {
+    this._octavePicker.addEventListener('change', () => {
   
+      let keyboardOctaves = this._keyboard.querySelectorAll('.octave')
+
+        //removes previous keyboard octaves
+      keyboardOctaves.forEach(octave => {
+        this._keyboard.removeChild(octave)
+      })
+
+      this._noteFreq = this.createNoteTable(Number(this._octavePicker.value))
+      this.createKeyboard()
+      
+    }, false)
+
+   }
+
       /**
    * Creates array of pitches
    *
    * @memberof Synth
    */
-  createNoteTable(){
-    let noteFreq = [];
-    for (let i=0; i< 9; i++) {
-      noteFreq[i] = [];
-    }
-    
-  noteFreq[2]["C"] = 65.406391325149658;
-  noteFreq[2]["C#"] = 69.295657744218024;
-  noteFreq[2]["D"] = 73.416191979351890;
-  noteFreq[2]["D#"] = 77.781745930520227;
-  noteFreq[2]["E"] = 82.406889228217482;
-  noteFreq[2]["F"] = 87.307057858250971;
-  noteFreq[2]["F#"] = 92.498605677908599;
-  noteFreq[2]["G"] = 97.998858995437323;
-  noteFreq[2]["G#"] = 103.826174394986284;
-  noteFreq[2]["A"] = 110.000000000000000;
-  noteFreq[2]["A#"] = 116.540940379522479;
-  noteFreq[2]["B"] = 123.470825314031027;
+   createNoteTable (octave) {
 
-  noteFreq[3]["C"] = 130.812782650299317;
-  noteFreq[3]["C#"] = 138.591315488436048;
-  noteFreq[3]["D"] = 146.832383958703780;
-  noteFreq[3]["D#"] = 155.563491861040455;
-  noteFreq[3]["E"] = 164.813778456434964;
-  noteFreq[3]["F"] = 174.614115716501942;
-  noteFreq[3]["F#"] = 184.997211355817199;
-  noteFreq[3]["G"] = 195.997717990874647;
-  noteFreq[3]["G#"] = 207.652348789972569;
-  noteFreq[3]["A"] = 220.000000000000000;
-  noteFreq[3]["A#"] = 233.081880759044958;
-  noteFreq[3]["B"] = 246.941650628062055;
+    let noteFreq = []
+     
+    for (let i = 0; i < 2; i++) {
+       noteFreq[octave + i] = []
+    }
+     
+    noteFreq[octave]['C'] = 32.703195662574829 
+    noteFreq[octave]['C#'] = 34.647828872109012
+    noteFreq[octave]['D'] = 36.708095989675945
+    noteFreq[octave]['D#'] = 38.890872965260113
+    noteFreq[octave]['E'] = 41.203444614108741
+    noteFreq[octave]['F'] = 43.653528929125485
+    noteFreq[octave]['F#'] = 46.249302838954299
+    noteFreq[octave]['G'] = 48.999429497718661
+    noteFreq[octave]['G#'] = 51.913087197493142
+    noteFreq[octave]['A'] = 55.000000000000000
+    noteFreq[octave]['A#'] = 58.270470189761239
+    noteFreq[octave]['B'] = 61.735412657015513
+
+    for(var pitch in noteFreq[octave]){
+      noteFreq[octave][pitch] = noteFreq[octave][pitch] * octave
+      noteFreq[octave + 1][pitch] = noteFreq[octave][pitch] * 2
+    }
 
     return noteFreq
-  }
+   }
 
-  setup(){
-    this._noteFreq = this.createNoteTable()
-    this._masterGainNode = this._audioContext.createGain()
-    this._masterGainNode.connect(this._audioContext.destination)
-    this._masterGainNode.gain.value = this._volumeControl.value
-
-    this._volumeControl.addEventListener("change", ()=>{
-      this.changeVolume()
-    }, false);
-
+   createKeyboard(){
     this._noteFreq.forEach((keys, idx) => {
       let keyList = Object.entries(keys)
-      let octaveElem = document.createElement("div")
-      octaveElem.className = "octave"
-      
-      keyList.forEach((key)=> {
-          octaveElem.appendChild(this.createKey(key[0], idx, key[1]))
-      });
+      let octaveElem = document.createElement('div')
+      octaveElem.className = 'octave'
+
+      keyList.forEach((key) => {
+        octaveElem.appendChild(this.createKey(key[0], idx, key[1]))
+      })
       this._keyboard.appendChild(octaveElem)
-    });
-    
-    this._sineTerms = new Float32Array([0, 0, 1, 0, 1]);
-    this._cosineTerms = new Float32Array(this._sineTerms.length);
-    this._customWaveform = this._audioContext.createPeriodicWave(this._cosineTerms, this._sineTerms)
+    })
 
-    for (let i = 0; i < 9; i++) {
-      this._oscList[i] = []
-    } 
-  }
+   }
 
-  changeVolume(){
-    this._masterGainNode.gain.value = this._volumeControl.value
-  }
+   setup () {
+     this._noteFreq = this.createNoteTable(3)
+     this._masterGainNode = this._audioContext.createGain()
+     this._masterGainNode.connect(this._audioContext.destination)
+     this._masterGainNode.gain.value = this._volumeControl.value
 
-  createKey(note, octave, freq){
-    let keyElement = document.createElement("div")
-    let labelElement = document.createElement("div")
-   
-    keyElement.className = "key"
-    keyElement.dataset["octave"] = octave
-    keyElement.dataset["note"] = note
-    keyElement.dataset["frequency"] = freq
-    labelElement.innerHTML = note + "<sub>" + octave + "</sub>"
-    keyElement.appendChild(labelElement)
-  
-    keyElement.addEventListener("mousedown", event =>{
-      this.notePressed(keyElement)  
-    }, false)
-    keyElement.addEventListener("mouseup", event =>{
-      this.noteReleased(keyElement) 
-    }, false)
-    keyElement.addEventListener("mouseover", event =>{
-      this.noteReleased(keyElement) 
-    }, false)
-    keyElement.addEventListener("mouseleave", event =>{
-      this.noteReleased(keyElement) 
-    }, false)
-  
-    return keyElement
-  }
+     this._volumeControl.addEventListener('change', () => {
+       this.changeVolume()
+     }, false)
 
-  playTone(freq) {
-    let osc = this._audioContext.createOscillator()
-    osc.connect(this._masterGainNode)
-   
-    let type = this._wavePicker.options[this._wavePicker.selectedIndex].value
-   
-    if (type == "custom") {
-      osc.setPeriodicWave(this._customWaveform)
-    } else {
-      osc.type = type
-    }
-  
-    osc.frequency.value = freq
-    osc.start()
-   
-    return osc
-  }
+     this.createKeyboard()
 
-  notePressed(keyElement) {
-    let dataset = keyElement.dataset
-    if (!dataset["pressed"]) {
-      this._oscList[dataset["octave"][dataset["note"]]] = this.playTone(dataset["frequency"])
-      dataset["pressed"] = "yes"
-    }
-  }
+     this._sineTerms = new Float32Array([0, 0, 1, 0, 1])
+     this._cosineTerms = new Float32Array(this._sineTerms.length)
+     this._customWaveform = this._audioContext.createPeriodicWave(this._cosineTerms, this._sineTerms)
 
-  noteReleased(keyElement) {
-    let dataset = keyElement.dataset
-    if (dataset && dataset["pressed"]) {
-      this._oscList[dataset["octave"][dataset["note"]]].stop()
-      this._oscList[dataset["octave"][dataset["note"]]] = null
-      delete dataset["pressed"]
-    }
-  }
+     for (let i = 0; i < 9; i++) {
+       this._oscList[i] = []
+     }
+   }
+
+   changeVolume () {
+     this._masterGainNode.gain.value = this._volumeControl.value
+   }
+
+   createKey (note, octave, freq) {
+     let keyElement = document.createElement('div')
+     let labelElement = document.createElement('div')
+
+     keyElement.className = 'key'
+     keyElement.dataset['octave'] = octave
+     keyElement.dataset['note'] = note
+     keyElement.dataset['frequency'] = freq
+
+     if(note.length > 1){
+       keyElement.setAttribute('class', 'sharpKey')
+     }
+
+
+     labelElement.innerHTML = note + '<sub>' + octave + '</sub>'
+     
+     keyElement.appendChild(labelElement)
+
+     keyElement.addEventListener('mousedown', event => {
+       this.notePressed(keyElement)
+     }, false)
+     keyElement.addEventListener('mouseup', event => {
+       this.noteReleased(keyElement)
+     }, false)
+     keyElement.addEventListener('mouseover', event => {
+       this.noteReleased(keyElement)
+     }, false)
+     keyElement.addEventListener('mouseleave', event => {
+       this.noteReleased(keyElement)
+     }, false)
+
+     return keyElement
+   }
+
+   playTone (freq) {
+     let osc = this._audioContext.createOscillator()
+     osc.connect(this._masterGainNode)
+
+     let type = this._wavePicker.options[this._wavePicker.selectedIndex].value
+
+     if (type === 'custom') {
+       osc.setPeriodicWave(this._customWaveform)
+     } else {
+       osc.type = type
+     }
+
+     osc.frequency.value = freq
+     osc.start()
+
+     return osc
+   }
+
+   notePressed (keyElement) {
+     let dataset = keyElement.dataset
+     if (!dataset['pressed']) {
+       this._oscList[dataset['octave'][dataset['note']]] = this.playTone(dataset['frequency'])
+       dataset['pressed'] = 'yes'
+     }
+   }
+
+   noteReleased (keyElement) {
+     let dataset = keyElement.dataset
+     if (dataset && dataset['pressed']) {
+       this._oscList[dataset['octave'][dataset['note']]].stop()
+       this._oscList[dataset['octave'][dataset['note']]] = null
+       delete dataset['pressed']
+     }
+   }
 }
 
-  
-window.customElements.define('synth-element', Synth)
+ window.customElements.define('synth-element', Synth)
 
-module.exports = Synth
+ module.exports = Synth
